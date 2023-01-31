83055b8ffcd285298f93a104fd3ff8293b5eb79f
VaneCarreras <vanecarreras91@gmail.com>
2023-01-31 03:47:48 -0300

    m

---
+++ BackEnd/src/main/java/com/portfolio/vane/VaneApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.vane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaneApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/vane/VaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.vane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
